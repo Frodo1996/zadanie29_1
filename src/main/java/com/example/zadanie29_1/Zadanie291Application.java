e573b298f426a9489e216b138678253e0a2a119b
Frodo1996 <frodson35@gmail.com>
2024-01-16 19:01:20 +0100

    początek zadania

---
+++ src/main/java/com/example/zadanie29_1/Zadanie291Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie29_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie291Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie291Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zadanie29_1/Zadanie291ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie29_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie291ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
